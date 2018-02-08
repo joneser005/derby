ad7c9f36ebb0dfbc5dbb64e8abe0e7dce876ecc1
Robb Jones <robb.kc.jones@gmail.com>
2018-02-08 08:33:20 -0600

    Updated for CLI, potential admin form integration (#30)

--- runner/name_generator.py
+++ runner/name_generator.py
@@ -3,23 +3,21 @@ Created on Jan 10, 2014
 
 @author: robb
 '''
+
+import argparse
 import logging
 import random
-
 import os
 
 log = logging.getLogger('runner')
 
-nouns = []
-adjectives = []
-
 
 def load(reload_files=False,
-         fnoun=os.path.join(CURRENT_PATH, '../sql/nouns.txt'),
-         fadj=os.path.join(CURRENT_PATH, '../sql/adjectives.txt')):
-    global nouns
-    global adjectives
+         fnoun=os.path.join(os.getcwd(), '../sql/nouns.txt'),
+         fadj=os.path.join(os.getcwd(), '../sql/adjectives.txt')):
 
+    nouns = []
+    adjectives = []
     if reload_files or nouns is None or 0 == len(nouns):
         with open(fnoun) as f:
             nouns = f.read().splitlines()
@@ -29,50 +27,60 @@ def load(reload_files=False,
             adjectives = f.read().splitlines()
             log.info('Loaded {} adjectives from {}'.format(len(adjectives), fadj))
 
+    return nouns, adjectives
 
-def make_names(pair_count=3):
+
+def make_names(nouns, adjectives, pair_count: int=3):
     ''' Returns a list of tuple pairs for use in Django's admin 'choices' drop-list. '''
-    n = random.sample(nouns, pair_count)
-    a = random.sample(adjectives, pair_count)
     result = []
+    n = random.choices(nouns, k=pair_count)
+    a = random.choices(adjectives, k=pair_count)
     for x in range(pair_count):
-        name1 = getFormat1(n[x], a[x])
-        result.append((name1, name1))
-        name2 = name1 = getFormat2(n[x], a[x])
-        result.append((name2, name2))
+        if random.choice([True, False]):
+            name1 = name_format2(n[x], a[x])
+            result.append((name1, name1))
+        else:
+            name2 = name1 = name_format1(n[x], a[x])
+            result.append((name2, name2))
     return result
 
 
-def getFormat1(n, a):
+def name_format1(n, a):
     return 'The ' + a + ' ' + n
 
 
-def getFormat2(n, a):
+def name_format2(n, a):
     return n + ' the ' + a
 
 
-def getRandomName():
+def get_random_name():
     names = make_names(1)
     return random.sample(list(names), 1)
 
 
-def getInsert(name):
-    return 'insert into runner_racername(name) values ("{}");\n'.format(name)
+def format_sql_insert(name):
+    return f'insert into runner_racername(name) values ("{name}");\n'
 
 
-def main():
-    print('Loading values...')
-    load()
-    #     print('Generating 100 name pairs...')
-    #     for x in range(100):
-    #         print make_names()
-    print('Generating sql inserts...')
-    with open(os.path.join(CURRENT_PATH, '../sql/name_inserts.sql'), 'w') as f:
-        for n in nouns:
-            for a in adjectives:
-                f.write(getInsert(getFormat1(n, a)))
-                f.write(getInsert(getFormat2(n, a)))
+def format_text(name):
+    return f'{name}'
+
+
+def emit_names(names, fmt):
+    outputf = format_sql_insert if fmt == 'sql' else format_text
+    for name in names:
+        print(outputf(name))
+
+
+def main(n: int=3, fmt='text'):
+    nouns, adjectives = load()
+    names = make_names(nouns, adjectives, n)
+    emit_names(names, fmt)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('n', type=int, help='Number of names to generate')
+    parser.add_argument('output', choices=['text', 'sql'])
+    args = parser.parse_args()
+    main(args.n, args.output)
